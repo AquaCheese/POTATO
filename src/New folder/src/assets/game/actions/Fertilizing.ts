27df662b065a230749001ac6a428cdbae241396c
AquaCheese <aquacheese.yt@gmail.com>
2025-06-08 22:51:01 +0100

    Remove duplicate New folder project and clean up workspace

--- src/New folder/src/assets/game/actions/Fertilizing.ts
+++
@@ -1,8 +0,0 @@
-import { PotatoField } from '../PotatoField';
-
-export class Fertilizing {
-    execute(field: PotatoField) {
-        // Logic to fertilize the potato field
-        field.fertilize(1); // Default to fertilizing with 1 unit
-    }
-}
